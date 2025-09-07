9a9f9ed94998bfe09e2a5373fe689f7b495a6147
Isaac Miti <ikayz@live.com>
2025-09-07 22:41:14 +0200

    Added signs to stretch the exercise

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -30,7 +30,32 @@ class Program
             letter = "F";
         }
 
-        Console.WriteLine($"Your letter grade is: {letter}");
+        string sign = "";
+        int lastDigit = grade % 10;
+
+        // Only add sign for A, B, C, D
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            // No A+
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+        }
+        // No sign for F
+        Console.WriteLine($"Your letter grade is: {letter}{sign}");
 
         if (grade >= 70)
         {
